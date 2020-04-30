2db63b663bb424abaa1a6302144c6077c9760c8c
TheCats100 <m-g-152@outlook.fr>
2020-04-30 09:40:41 +0200

    feat(loadingspinner): add beautiful features

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import React, { useState } from 'react';
 import Question from './Components/Question';
-import Help from './Components/Help'
+import Help from './Components/Help';
 import './App.css';
 import './Components/Question.css';
 
 function App() {
   const [init, setInit] = useState(false);
   return (
-
-    <div className="page">
-      <div>
-        <h1 className="title">WhereCam</h1>
+    <>
+      <div className="page">
+        <div>
+          <h1 className="title">WhereCam</h1>
+        </div>
+        <div className="accueilPage">
+          {init === false
+            ? <button className="buttonPlay" type="button" onClick={() => setInit(true)}>PLAY</button>
+            : <Question />}
+        </div>
       </div>
-      <div className="accueilPage" >
-
-        {init === false
-          ? <button className="buttonPlay" type="button" onClick={() => setInit(true)}>PLAY</button>
-          : <Question />}
-      </div>
-
-    </div>
-
       <div>
         <Help />
       </div>
--- src/Components/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { css } from '@emotion/core';
+import ClockLoader from 'react-spinners/ClockLoader';
 
-const LoadingSpinner = () => (
-  <div>
-    <i className="fa fa-spinner fa-spin" />
-    Loading...
-  </div>
-);
+const clockloader = css`
+  display: block;
+  margin: 0 auto;
+  border-color: blue;
+`;
+class LoadingSpinner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+ 
+  render() {
+    const { loading } = this.state;
+    return (
+      <div className="sweet-loading">
+        <ClockLoader
+          css={clockloader}
+          size={250}
+          color="#123abc"
+          loading={loading}
+        />
+      </div>
+    );
+  }
+}
 
 export default LoadingSpinner;
