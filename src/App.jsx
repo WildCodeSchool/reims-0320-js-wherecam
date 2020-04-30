51c2c2c88a135fea58127510b9e238a35d376fc4
TheCats100 <m-g-152@outlook.fr>
2020-04-30 09:42:37 +0200

    Revert "feat(loadingspinner): add beautiful features"
    This reverts commit 2db63b663bb424abaa1a6302144c6077c9760c8c.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import Question from './Components/Question';
-import Help from './Components/Help';
+import Help from './Components/Help'
 import './App.css';
 import './Components/Question.css';
 
 function App() {
   const [init, setInit] = useState(false);
   return (
-    <>
-      <div className="page">
-        <div>
-          <h1 className="title">WhereCam</h1>
-        </div>
-        <div className="accueilPage">
-          {init === false
-            ? <button className="buttonPlay" type="button" onClick={() => setInit(true)}>PLAY</button>
-            : <Question />}
-        </div>
+
+    <div className="page">
+      <div>
+        <h1 className="title">WhereCam</h1>
       </div>
+      <div className="accueilPage" >
+
+        {init === false
+          ? <button className="buttonPlay" type="button" onClick={() => setInit(true)}>PLAY</button>
+          : <Question />}
+      </div>
+
+    </div>
+
       <div>
         <Help />
       </div>
--- src/Components/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner.jsx
@@ -1,33 +1,10 @@
 import React from 'react';
-import { css } from '@emotion/core';
-import ClockLoader from 'react-spinners/ClockLoader';
 
-const clockloader = css`
-  display: block;
-  margin: 0 auto;
-  border-color: blue;
-`;
-class LoadingSpinner extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
-  }
- 
-  render() {
-    const { loading } = this.state;
-    return (
-      <div className="sweet-loading">
-        <ClockLoader
-          css={clockloader}
-          size={250}
-          color="#123abc"
-          loading={loading}
-        />
-      </div>
-    );
-  }
-}
+const LoadingSpinner = () => (
+  <div>
+    <i className="fa fa-spinner fa-spin" />
+    Loading...
+  </div>
+);
 
 export default LoadingSpinner;
